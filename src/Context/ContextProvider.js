86ac1f68684bda1ab860efe055c5ca76234d1a3e
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-11-25 23:29:02 +0600

    context provider added

--- src/Context/ContextProvider.js
+++ src/Context/ContextProvider.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import app from '../firebase.config';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
-
+export const authContext = createContext(app);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
 const ContextProvider = ({ children }) => {
+
+
+
+
+    const authInfo = {};
     return (
         <div>
-
+            <authContext.Provider value={authInfo}>
+                {children}
+            </authContext.Provider>
         </div>
     );
 };
